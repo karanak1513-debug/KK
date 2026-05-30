38bccc40af41437293f375bfd53f70480b149feb
karanak1513-debug <karanak1513-debug@users.noreply.github.com>
2026-05-30 18:46:20 +0530

    Make contact page map URL dynamic and update Firebase coordinates

--- kk-moulding/app/(site)/contact/page.tsx
+++ kk-moulding/app/(site)/contact/page.tsx
@@ -113,7 +113,7 @@ export default function ContactPage() {
               {/* Map */}
               <div className="mt-8 aspect-video w-full bg-[#FAFAFA] overflow-hidden border border-[#E6D5C3]">
                 <iframe
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d118447.92896374838!2d70.75119!3d22.30390!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3959ca4d0c0f0f0f%3A0x5a93938f7ab44b7f!2sRajkot%2C%20Gujarat!5e0!3m2!1sen!2sin!4v1700000000000!5m2!1sen!2sin"
+                  src={mapEmbedUrl}
                   width="100%"
                   height="100%"
                   style={{ border: 0, filter: 'grayscale(0.5) contrast(1.1) brightness(1.1) sepia(0.1)' }}
